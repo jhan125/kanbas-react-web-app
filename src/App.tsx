06871726855a3a7054def082cf0711661a74d2ca
jhan125 <oliviahan125@gmail.com>
2024-09-12 17:22:53 -0700

    get start with a1

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to Web App!</p>
+        <p>Welcome to Web App</p>
       </header>
     </div>
   );
