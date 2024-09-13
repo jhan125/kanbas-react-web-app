6b01dcf1e7b85c51868a33d0b905ee043f1a4cf8
jhan125 <oliviahan125@gmail.com>
2024-09-12 21:47:57 -0700

    complete lab1

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,12 +1,14 @@
+import { Route, Routes, Navigate } from "react-router"
+import Account from "./Account";
+
 export default function Kanbas() {
-
   return (
-
     <div id="wd-kanbas">
-
-      <h1>Kanbas</h1>
-
+      <h1>Kanbas</h1>\
+      <Routes>
+        <Route path="/" element={<Navigate to="Account" />} />
+        <Route path="/Account/*" element={<Account />} />
+      </Routes>
     </div>
-
   );
 }
--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -1,3 +1,6 @@
+// Below line is temporarily used to ignore the warning that "img elements must have an alt prop, either with meaningful text, or an empty string for decorative images"
+/* eslint-disable jsx-a11y/alt-text */
+
 export default function Lab1() {
     return (
         <div id="wd-lab1">
@@ -130,6 +133,136 @@ export default function Lab1() {
                 </table>
             </div>
 
+            <div id="wd-images">
+                <h4>Image tag</h4>
+                Loading an image from the internet:
+                <br />
+                <img id="wd-starship"
+                    width="400px"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
+                <br />
+                Loading a local image:
+                <br />
+                <img id="wd-teslabot" src="../../../public/images/teslabot.jpg" height="200px" />
+            </div>
+
+            <div id="wd-forms">
+                <h4>Form Elements</h4>
+                <form id="wd-text-fields">
+                    <h5>Text Fields</h5>
+                    {/* placeholder and title give info hints. Value attribute is optional. */}
+                    <label htmlFor="wd-text-fields-username">Username:</label>
+                    <input id="wd-text-fields-username" placeholder="jdoe" /> <br />
+                    <label htmlFor="wd-text-fields-password">Password:</label>
+                    <input type="password" id="wd-text-fields-password" value="123@#$asd" />
+                    <br />
+                    <label htmlFor="wd-text-fields-first-name">First name:</label>
+                    <input type="text" id="wd-text-fields-first-name" title="John" /> <br />
+                    <label htmlFor="wd-text-fields-last-name">Last name:</label>
+                    <input type="text" id="wd-text-fields-last-name" placeholder="Doe"
+                        value="Wonderland" title="The last name" />
+
+
+                    <h5>Text boxes</h5>
+                    {/* Textarea tag -> long form text with configured width height. 
+                        Placeholder, tooltip -> give hints. Default value is in tag's body. */}
+                    <label>Biography:</label><br />
+                    <textarea id="wd-textarea" cols={30} rows={10}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </textarea>
+
+
+                    <h5 id="wd-buttons">Buttons</h5>
+                    <button id="wd-all-good" onClick={() => alert("Life is Good!")} type="button">
+                        Hello World!
+                    </button>
+
+
+                    <h5>File upload</h5>
+                    <input id="wd-upload" type="file" />
+
+
+                    <h5 id="wd-radio-buttons">Radio buttons</h5>
+                    {/* use the input tag's checkbox type to declare a checkbox.
+                        use checked to set the checkbox's as initially checked. */}
+
+                    <label>Favorite movie genre:</label><br />
+                    <input type="radio" name="radio-genre" id="wd-radio-comedy" />
+                    <label htmlFor="wd-radio-comedy">Comedy</label><br />
+                    <input type="radio" name="radio-genre" id="wd-radio-drama" />
+                    <label htmlFor="wd-radio-drama">Drama</label><br />
+                    <input type="radio" name="radio-genre" id="wd-radio-scifi" />
+                    <label htmlFor="wd-radio-scifi">Science Fiction</label><br />
+                    <input type="radio" name="radio-genre" id="wd-radio-fantasy" />
+                    <label htmlFor="wd-radio-fantasy">Fantasy</label>
+
+                    <h5 id="wd-checkboxes">Checkboxes</h5>
+                    <label>Favorite movie genre:</label><br />
+                    <input type="checkbox" name="check-genre" id="wd-chkbox-comedy" />
+                    <label htmlFor="wd-chkbox-comedy">Comedy</label><br />
+                    <input type="checkbox" name="check-genre" id="wd-chkbox-drama" />
+                    <label htmlFor="wd-chkbox-drama">Drama</label><br />
+                    <input type="checkbox" name="check-genre" id="wd-chkbox-scifi" />
+                    <label htmlFor="wd-chkbox-scifi">Science Fiction</label><br />
+                    <input type="checkbox" name="check-genre" id="wd-chkbox-fantasy" />
+                    <label htmlFor="wd-chkbox-fantasy">Fantasy</label>
+
+                    <h4 id="wd-dropdowns">Dropdowns</h4>
+                    {/*  Wrap several option tags in a select tag. 
+                         Optionally provide option's value, otherwise the option's text is the value of the select element. */}
+                    <h5>Select one</h5>
+                    <label htmlFor="wd-select-one-genre"> Favorite movie genre: </label><br />
+                    <select id="wd-select-one-genre">
+                        <option value="COMEDY">Comedy</option>
+                        <option value="DRAMA">Drama</option>
+                        <option selected value="SCIFI">Science Fiction</option>
+                        <option value="FANTASY">Fantasy</option>
+                    </select>
+
+                    <h5>Select many</h5>
+                    {/* Alternatively use attribute multiple to allow selecting more than one option.        
+                        Use ctrl+click to select more than one option */}
+                    <label htmlFor="wd-select-many-genre"> Favorite movie genres: </label><br />
+                    <select id="wd-select-many-genre" multiple>
+                        <option selected value="COMEDY">Comedy</option>
+                        <option value="DRAMA">Drama</option>
+                        <option selected value="SCIFI">Science Fiction</option>
+                        <option value="FANTASY">Fantasy</option>
+                    </select>
+
+
+                    <h4>Other HTML field types</h4>
+
+                    <label htmlFor="wd-text-fields-email"> Email: </label>
+                    <input type="email"
+                        placeholder="jdoe@somewhere.com"
+                        id="wd-text-fields-email" /><br />
+
+                    <label htmlFor="wd-text-fields-salary-start"> Starting salary:
+                    </label>
+                    <input type="number"
+                        id="wd-text-fields-salary-start"
+                        placeholder="1000"
+                        value="100000" /><br />
+
+                    <label htmlFor="wd-text-fields-rating"> Rating: </label>
+                    <input type="range" id="wd-text-fields-rating"
+                        placeholder="Doe"
+                        max="5"
+                        value="4" /><br />
+
+                    <label htmlFor="wd-text-fields-dob"> Date of birth: </label>
+                    <input type="date"
+                        id="wd-text-fields-dob"
+                        value="2000-01-21" /><br />
+
+                </form>
+            </div>
+
+            <h4>Anchor tag</h4>
+            {/* Navigate to other websites/pages within the same website. */}
+            Please <a id="wd-lipsum" href="https://www.lipsum.com">click here</a> to get dummy text<br />
+            Click <a id="wd-github" href="https://github.com/jhan125/kanbas-react-web-app/tree/a1">this link</a> to view code on GitHub
         </div>
     );
 }
